2424e0b3bc4787a847e011a3dacef9e21a43b025
Ashish Kumar <asis4911@gmail.com>
2020-11-10 23:44:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,url_for,request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('app.html')
+
+@app.route('/send', methods=['POST'])
+def send(sum=sum):
+    if request.method == 'POST':
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        operation = request.form['operation']
+        
+        if operation =='add':
+            sum = float(num1) + float(num2)
+            return render_template('app.html', sum=sum)
+        
+        elif operation =='subtract':
+            sum = float(num1) - float(num2)
+            return render_template('app.html',sum=sum)
+        
+        elif operation =='multiply':
+            sum = float(num1) * float(num2)
+            return render_template('app.html',sum=sum)
+        
+        elif operation =='divide':
+            sum = float(num1) / float(num2)
+            return render_template('app.html',sum=sum)
+    
+
+if __name__=='__main__':
+    app.debug = False
+    app.run()
